fcf31c3733770bbe5bf7fcae863332ed0c5ff3e1
Dragan Djuric <dragandj@gmail.com>
2022-02-09 01:09:34 +0100

    Added license notice and project description.

---
+++ README.md
@@ -0,0 +1,23 @@
+[New books available for subscription](https://aiprobook.com)
+
+<img src="http://aiprobook.com/img/dlfp-cover.png" alt="Deep Learning for Programmers" title="Deep Learning for Programmers" align="left" width="250"/>
+
+<img src="http://aiprobook.com/img/lafp-cover.png" alt="Numerical Linear Algebra for Programmers" title="Numerical Linear Algebra for Programmers" align="right" width="250"/>
+
+# Clojure Sound
+
+[Adopt your pet function](https://dragan.rocks/articles/18/Patreon-Announcement-Adopt-a-Function) and [become a patron](https://patreon.com/draganrocks).
+
+Clojure Sound is a library for transforming digital media and communicating with MIDI devices.
+It is based on Java Sound API, but improves on Java Sound's UX shortcomings as much as possible,
+to provide nice interactive programming experience.
+
+## How to use it
+
+TODO
+
+## License
+
+Copyright © 2022-2022 Dragan Djuric
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- src/uncomplicate/clojure_sound/midi.clj
+++ src/uncomplicate/clojure_sound/midi.clj
@@ -1,3 +1,11 @@
+;;   Copyright (c) Dragan Djuric. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) or later
+;;   which can be found in the file LICENSE at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 (ns uncomplicate.clojure-sound.midi
   (:refer-clojure :exclude [sequence])
   (:require [uncomplicate.commons.core :refer [Releaseable]]
@@ -36,8 +44,7 @@
 ;; ===================== Keyword coding ================================================
 
 (def sync-mode
-  {:internal-clock Sequencer$SyncMode/INTERNAL_CLOCK
-   :internal Sequencer$SyncMode/INTERNAL_CLOCK
+  {:internal Sequencer$SyncMode/INTERNAL_CLOCK
    :sync Sequencer$SyncMode/MIDI_SYNC
    :time Sequencer$SyncMode/MIDI_TIME_CODE
    :no-sync Sequencer$SyncMode/NO_SYNC})
